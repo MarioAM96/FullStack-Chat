ead18ba362a4a0d61c6a3e8fb382b799a551f188
MarioAM96 <91484233+MarioAM96@users.noreply.github.com>
2023-04-12 14:09:07 -0500

    Final V

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,5 +1,16 @@
-const ChatsPage = () => {
-    return <>chats...</>
-}
+import { PrettyChatWindow } from "react-chat-engine-pretty";
 
-export default ChatsPage
+const ChatsPage = (props) => {
+  return (
+    <div style={{ height: '100vh'}}>
+      <PrettyChatWindow
+        projectId='b80619a4-4a1a-4301-a7f1-22d3d2b3b084'
+        username={props.user.username}
+        secret={props.user.secret}
+        style={{height: '100%'}}
+      />
+    </div>
+  );
+};
+
+export default ChatsPage;
